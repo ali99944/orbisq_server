7e9c6a2b9da73bc68da917a73344c519913f4ee0
ali99944 <alitarek99944@gmail.com>
2025-05-12 05:37:21 +0300

    update

--- routes/product_route.js
+++ routes/product_route.js
@@ -7,7 +7,7 @@ import {
     deleteProductController
 } from "../controllers/product_controller.js";
 import { createMulterStorage } from "../lib/multer_storage.js"; // Your multer setup
-import { authenticatePortal } from "../middlewares/shop_auth_middleware.js";
+// import { authenticatePortal } from "../middlewares/shop_auth_middleware.js";
 
 const router = express.Router();
 
@@ -15,7 +15,7 @@ const router = express.Router();
 const productUpload = createMulterStorage('images/products', 'product').single('image');
 // Note: 'image' is the field name expected in the form-data
 
-router.post('/products', authenticatePortal, productUpload, createProductController);
+router.post('/products', productUpload, createProductController);
 router.get('/products', getAllProductsController);
 router.get('/products/:id', getProductByIdController);
 router.put('/products/:id', productUpload, updateProductController);
