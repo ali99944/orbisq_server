c0b9a1959a9c2a0336cbe133aa99d9bedce00196
ali99944 <alitarek99944@gmail.com>
2025-05-13 22:36:25 +0300

    update

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.use('/storage', express.static(path.join(__dirname, './public')))
+app.use('/public', express.static(path.join(__dirname, './public')))
 
 app.use(compression())
 app.use(cookieParser())
